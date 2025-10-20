008b9471811e5d33b79ffac453a4e444c10fff08
Diego Mendoza <diego.sanchez33@uabc.edu.mx>
2025-10-19 21:10:53 -0700

    commit capa vista

--- persistencia/src/main/java/mx/avanti/desarollo/integration/ServiceLocator.java
+++ persistencia/src/main/java/mx/avanti/desarollo/integration/ServiceLocator.java
@@ -1,46 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.avanti.desarollo.integration;
 
 import jakarta.persistence.EntityManager;
 import mx.avanti.desarollo.dao.*;
 import mx.avanti.desarollo.persistence.HibernateUtil;
 
-
-/**
- *
- * @author total
- */
 public class ServiceLocator {
-    private static UsuarioDAO usuarioDAO;
-    private static ArticuloDAO articuloDAO;
 
     private static EntityManager getEntityManager(){
         return HibernateUtil.getEntityManager();
     }
 
-    /**
-     * se crea la instancia de usuarioDAO si esta no existe
-     */
+
     public static UsuarioDAO getInstanceUsuarioDAO(){
-        if(usuarioDAO == null){
-            usuarioDAO = new UsuarioDAO(getEntityManager());
-            return usuarioDAO;
-        } else{
-            return usuarioDAO;
-        }
+        return new UsuarioDAO(getEntityManager());
     }
 
     public static ArticuloDAO getInstanceArticuloDAO(){
-        if(articuloDAO == null){
-            articuloDAO = new ArticuloDAO(getEntityManager());
-            return articuloDAO;
-        } else{
-            return articuloDAO;
-        }
+        return new ArticuloDAO(getEntityManager());
     }
-
 }
