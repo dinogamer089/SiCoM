996ad6a644bb6b68148bb42b2f0084dbeb5999b6
dinog <dinogamer089@gmail.com>
2025-11-10 08:09:01 -0800

    Manejo de las nuevas propiedades de Articulo e Imagen

--- negocio/src/main/java/mx/desarollo/integration/ServiceFacadeLocator.java
+++ negocio/src/main/java/mx/desarollo/integration/ServiceFacadeLocator.java
@@ -1,11 +1,15 @@
 package mx.desarollo.integration;
 
+import mx.desarollo.facade.FacadeEmpleado;
 import mx.desarollo.facade.FacadeLogin;
 import mx.desarollo.facade.FacadeArticulo;
+import mx.desarollo.facade.FacadeRenta;
 
 public class ServiceFacadeLocator {
     private static FacadeLogin facadeLogin;
     private static FacadeArticulo facadeArticulo;
+    private static FacadeRenta facadeRenta;
+    private static FacadeEmpleado facadeEmpleado;
 
     public static FacadeLogin getInstanceFacadeLogin() {
         if (facadeLogin == null) {
@@ -19,5 +23,19 @@ public class ServiceFacadeLocator {
             facadeArticulo = new FacadeArticulo();
         }
         return facadeArticulo;
+    }
+
+    public static FacadeRenta getInstanceFacadeRenta() {
+        if (facadeRenta == null) {
+            facadeRenta = new FacadeRenta();
+        }
+        return facadeRenta;
+    }
+
+    public static FacadeEmpleado getInstanceFacadeEmpleado() {
+        if (facadeEmpleado == null) {
+            facadeEmpleado = new FacadeEmpleado();
+        }
+        return facadeEmpleado;
     }
 }
