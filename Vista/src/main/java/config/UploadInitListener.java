9d60cdb318831c46afb8fc0c578ad967d5148b83
Diego Mendoza <diego.sanchez33@uabc.edu.mx>
2026-05-25 11:42:59 -0700

    Commit Correcciones PBI-CA-US3

--- Vista/src/main/java/config/UploadInitListener.java
+++ Vista/src/main/java/config/UploadInitListener.java
@@ -2,9 +2,7 @@ package config;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
 
-@WebListener
 public class UploadInitListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
